ba26bb6ccfab9db2ec819b227c3ae53535262f77
Tsadiku-Mengist <102510229+Tsadiku-Mengist@users.noreply.github.com>
2022-12-08 22:17:51 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,23 +1,3 @@
-// ----- ----- Class 1: SocketType ----- -----
-
-
-
-public class SocketType {
-	public final static int SOCK_STREAM = SocketType2.SOCK_STREAM;
-}
-
-
-// ----- ----- Class 2: Socket ----- -----
-
-public class Socket {
-	
-	public final static int PF_INET = 2;
-
-	public final static int AF_INET = PF_INET;
-}
-
-
-// ----- ----- Class 3: Sockaddr ----- -----
 
 
 
