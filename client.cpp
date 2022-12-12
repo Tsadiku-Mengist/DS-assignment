72dfd424961c55f0abc78bc1a9bf49817a9dafd3
Tsadiku-Mengist <102510229+Tsadiku-Mengist@users.noreply.github.com>
2022-12-12 04:35:43 -0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -18,15 +18,37 @@ int sock;
 struct sockaddr_in cli_name;
 int count;
 int value;
+  cout<<"Client is alive and establishing socket connection.";
+     
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0){
+        perror ("Error opening channel");
+         close(sock);
+          exit(1);
+    }
+    bzero(&cli_name, sizeof(cli_name));
+    cli_name.sin_family = AF_INET;
+    cli_name.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+    cli_name.sin_port = htons(PORT);
+    
+     if (connect(sock, (struct sockaddr *)&cli_name, sizeof(cli_name)) < 0)
+    { perror ("Error establishing communications");
+      close(sock);
+      exit(1);
+    }
+     for (count = 1; count <= SIM_LENGTH; count++) {
+         read(sock, &value, 4);
+      cout<<"Client has received %d from socket" + value;
+         
+           cout<<"Exiting now";
 
+  close(sock);
+  exit(0);
+    }
 return 0;
 }
   
 
-void clean_up(int cond, int *sock){
-       cout<<"Exiting now";
-        close(*sock);
-         unlink(SOCKET_NAME);
-          exit(cond);
-}
+
+
 
