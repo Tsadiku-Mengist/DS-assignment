b7b388d3af9d8e28a49998b1ac1002f5ec23660f
Tsadiku-Mengist <102510229+Tsadiku-Mengist@users.noreply.github.com>
2022-12-12 04:15:59 -0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,7 +1,17 @@
+#include<sys/type> //header file defines a collection of typedef symbols and structures
+#include<unistd>  //header defines miscellaneous symbolic constants and types, and declares miscellaneous functions
+#include<stdio>  //header file which has the necessary information to include the input/output related functions in our program
+#include<string>
+#include<stdlib>
+#include<signal>
+#include<sys/socket>
+#include<sys/un>
+#include <iostream>
 
 using namespace std;
 #define PORT 1227
 #define SIM_LENGTH 10
+
 int main (){
   
  int sock; 
